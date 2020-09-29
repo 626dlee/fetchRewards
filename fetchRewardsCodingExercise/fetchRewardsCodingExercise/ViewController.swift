d85ff86ba52d954d1ea00bba68b9de60526f6acf
David Lee <626davidlee@gmail.com>
2020-09-29 01:13:57 -0700

    Grouping and display updates

--- fetchRewardsCodingExercise/fetchRewardsCodingExercise/ViewController.swift
+++ fetchRewardsCodingExercise/fetchRewardsCodingExercise/ViewController.swift
@@ -21,9 +21,34 @@ struct Item: Decodable {
 // MVC CONTROLLER
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    // Number of data objects
+    // Number of sections (grouped by List IDs)
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return listSortedById.keys.count
+    }
+    
+    // Disables highlighting of rows when clicked
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    // Number of data objects per List ID
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return sectionData[section].count
+    }
+    
+    // Section header
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UILabel()
+        let bgColor = UIColor(red: 0, green: 128/255, blue: 128/255, alpha: 1)
+        headerView.backgroundColor = bgColor
+        headerView.text = "\tList ID : \(sectionData[section][0].listId)"
+        headerView.shadowColor = .black
+        headerView.shadowOffset = CGSize.init(width: 1, height: 0)
+        
+        headerView.font = UIFont.systemFont(ofSize: 28, weight: .heavy)
+        headerView.textColor = .white
+        headerView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        return headerView
     }
     
     // Display each data object in a list
@@ -34,20 +59,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
         
         
-        cell?.textLabel?.text = list[indexPath.row].name
-        cell?.detailTextLabel?.text = "List ID: \(list[indexPath.row].listId)\tID: \(list[indexPath.row].id)"
-        cell?.textLabel?.textColor = .white
-        cell?.detailTextLabel?.textColor = .white
+        cell?.textLabel?.text = sectionData[indexPath.section][indexPath.row].name
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        cell?.detailTextLabel?.text = "ID:\t\(sectionData[indexPath.section][indexPath.row].id)"
+        cell?.detailTextLabel?.textAlignment = .left
+        cell?.textLabel?.textColor = .black
+        cell?.detailTextLabel?.textColor = .black
+        cell?.backgroundColor = .white
+        cell?.detailTextLabel?.translatesAutoresizingMaskIntoConstraints = false
+        cell?.detailTextLabel?.leadingAnchor.constraint(equalTo: (cell?.contentView.trailingAnchor)!, constant: -100).isActive = true
+        cell?.detailTextLabel?.centerYAnchor.constraint(equalTo: (cell?.contentView.centerYAnchor)!).isActive = true
         return cell!
     }
     
         
     
     let urlString = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
-    var list: [Item] = []
-    var differentListIds: [Int: Int] = [:]
-    let cellID: String = "cellid"
     
+    var list: [Item] = []
+    var listSortedById: [Int : [Item]] = [:]
+    let cellID: String = "cellid"
+    var sectionData = [[Item]]()
+    
+    
+  
     // MVC VIEW
     var mainTableView = UITableView()
     
@@ -69,12 +104,34 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             
             // Sort data again by item name
             self?.list.sort {
-                self?.differentListIds[$0.listId] = (self?.differentListIds[$0.listId] ?? 0) + 1
                 if $0.listId == $1.listId {
                     return $0.id < $1.id
                 }
                 return false
             }
+            
+            // Create separate lists for each List ID
+            self?.list.forEach {
+
+                if let _ = self?.listSortedById[$0.listId] {
+                    self?.listSortedById[$0.listId]?.append($0)
+                }
+                else {
+                    self?.listSortedById[$0.listId] = [$0]
+                }
+            }
+            
+            // Makes grouped object lists compatible with table-view structure
+            // Accounts for non-sequential list IDs
+            for (_, list) in self!.listSortedById {
+                self?.sectionData.append(list)
+            }
+            
+            // Orders the section data by list ID
+            self?.sectionData.sort {
+                $0[0].listId < $1[0].listId
+            }
+            
             
             // Update list with API data
             DispatchQueue.main.async {
@@ -89,6 +146,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         mainTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         mainTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         mainTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        mainTableView.layer.cornerRadius = 15
         
         mainTableView.delegate = self
         mainTableView.dataSource = self
@@ -105,11 +164,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 print("error: \(error)")
             } else {
                 if let response = response as? HTTPURLResponse {
+                    // HTTP Status Code
                     print("statusCode: \(response.statusCode)")
                 }
                 if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("data: \(data)")
+                    print("data: \(dataString)")
                     let decoder = JSONDecoder()
+                    // Send parsed JSON data to callback function
                     if let tempList = try? decoder.decode([Item].self, from: data) {
                         completion(tempList)
                     }
